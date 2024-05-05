04f9225d7c23ea6421dbe9e846b7d7d2477673e1
darkmarkdesign <115876728+darkmarkdesign@users.noreply.github.com>
2024-05-05 01:51:55 -0700

    Update animationLeave.js

--- src/animation/transition/animationLeave.js
+++ src/animation/transition/animationLeave.js
@@ -2,17 +2,26 @@ import gsap from 'gsap';
 
 export default function animationLeave(container) {
 	const transContainer = document.querySelector('.transition');
+	const head = document.querySelector('[data-animation="trans-head"]'),
 
 	const tl = gsap.timeline({
 		onComplete: () => {
 			gsap.set(transContainer, { transform: 'translateY(100%)' });
 		},
 	});
+	
+	tl.to(head('.char'),
+		{
+			yPercent: -100,
+			duration: 1.2,
+			ease: 'power4.out',
+			stagger: { each: 0.08 },
+		});	
 	tl.to(transContainer, {
 		transform: 'translateY(0%)',
 		duration: 1.9,
 		ease: 'expo.inOut',
-	});
+	}, 0.25);
 	tl.to(
 		container,
 		{
