ff4ab446347691c4bbe6304bc899403fd2dacb7b
darkmarkdesign <115876728+darkmarkdesign@users.noreply.github.com>
2024-05-05 05:30:07 -0700

    Update detailEnter.js

--- src/animation/transition/detailEnter.js
+++ src/animation/transition/detailEnter.js
@@ -2,12 +2,12 @@ import gsap from 'gsap';
 
 export default function detailEnter(container) {
 	const el = {
-		head: container.querySelectorAll('[data-animation="trans-head"]'),
-		text: container.querySelectorAll('[data-animation="trans-txt"]'),
-		detailCap: container.querySelectorAll('[data-animation="trans-sd"]'),
+		head: container.querySelector('[data-animation="trans-head"]'),
+		text: container.querySelector('[data-animation="trans-txt"]'),
+		detailCap: container.querySelector('[data-animation="trans-sd"]'),
 		transition: document.querySelector('.transition'),
 		whipe: container.querySelectorAll('.trans_whipe'),
-		img: container.querySelectorAll('.detail_img'),
+		img: container.querySelector('.detail_img'),
 	};
 
 	gsap.set(el.transition, { display: 'block' });
