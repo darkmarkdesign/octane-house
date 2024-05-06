d72ae4a1ff7989e4aa688b54d93eb6db4b4cdc88
darkmarkdesign <115876728+darkmarkdesign@users.noreply.github.com>
2024-05-06 05:07:31 -0700

    Update menuOpen.js

--- src/animation/menu/menuOpen.js
+++ src/animation/menu/menuOpen.js
@@ -1,5 +1,19 @@
 import gsap from 'gsap';
 import SmoothScroll from '../../utils/scroll';
+/* Other Animation Import */
+import Slider from '../slider/slider';
+import Split from '../../utils/split';
+import SmoothScroll from '../../utils/scroll';
+import Parallax from '../image/parallax';
+import Whipe from '../image/whipe';
+import Line from '../line/linein';
+import SlideUp from '../text/title';
+import SlideParagraph from '../text/paragraph';
+import FooterLogo from '../logo/footerLogo';
+import Fade from '../fade/fade';
+import Preloader from '../preloader/preloader';
+
+
 export default class MenuOpen {
 	constructor() {
 		this.el = {
