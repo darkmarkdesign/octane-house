037327d3a276802db80a7f1686c98755c69f6221
darkmarkdesign <115876728+darkmarkdesign@users.noreply.github.com>
2024-05-06 05:26:22 -0700

    Update index.js

--- src/animation/index.js
+++ src/animation/index.js
@@ -1,20 +1,5 @@
 import infoHover from './hover/infoHover';
-import gsap from 'gsap';
-import SmoothScroll from '../../utils/scroll';
-
-/* Other Animation Import */
-import Slider from '../slider/slider';
-import Split from '../../utils/split';
-
-import Parallax from '../image/parallax';
-import Whipe from '../image/whipe';
-import Line from '../line/linein';
-import SlideUp from '../text/title';
-import SlideParagraph from '../text/paragraph';
-import FooterLogo from '../logo/footerLogo';
-import Fade from '../fade/fade';
-import Preloader from '../preloader/preloader';
-
+import Slider from './slider/slider';
 import MenuOpen from './menu/menuOpen';
 
 export default class Animation {
