fa16f74b9ef4b3bb4ee67593a13c2acd0b6bd885
darkmarkdesign <115876728+darkmarkdesign@users.noreply.github.com>
2024-05-06 18:36:58 -0700

    Update index.js

--- src/animation/index.js
+++ src/animation/index.js
@@ -1,7 +1,7 @@
 import infoHover from './hover/infoHover';
 import Slider from './slider/slider';
 import MenuOpen from './menu/menuOpen';
-import Draggable from './slider/draggable';
+/* import Draggable from './slider/draggable'; */
 /* import detailEnter from './slider/detailEnter'; */
 
 export default class Animation {
@@ -12,7 +12,7 @@ export default class Animation {
 	init() {
 		new MenuOpen();
 		new Slider();
-		new Draggable();
+		/* new Draggable();*/
 		infoHover();
 	}
 }
